c78ba24178b1cfe91e50c6a9f8ea2dedc3c10d87
ObedAdhi <obedadhinatapraya@yahoo.com>
2021-01-21 18:44:15 +0700

    Setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const { listen } = require('socket.io')
+
+const app = require('epress')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const port = process.env.PORT || 3000
+
+io.on('connection', (socket) => {
+  console.log('Socket.io client connected');
+  // socket.emit()
+})
+
+server.listen(port, () => {
+  console.log(('Listening on port: ' + port));
+})
