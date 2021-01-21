f87a56b07dac55257cee5cd68d8ca5f95683deea
ObedAdhi <obedadhinatapraya@yahoo.com>
2021-01-22 04:14:29 +0700

    Basic room implementations

--- app.js
+++ app.js
@@ -3,22 +3,43 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const port = process.env.PORT || 3000
 
-let hp = 10
 let player = ''
+let roomname = []
+
 
 io.on('connection', (socket) => {
   console.log('Socket.io client connected');
-  socket.emit('init', hp)
+  socket.emit('roomname', roomname)
+  socket.on('addNewRoom', (payload) => {
+    roomname.push(payload)
+    io.emit('roomname', roomname)
+    socket.join(payload);
+  })
+
+  socket.on('goToRoom', (payload) => {
+    // buat join guard disini 
+    if (io.sockets.adapter.rooms[payload]) {
+      if (io.sockets.adapter.rooms[payload].length < 4) {
+        socket.join(payload);
+        io.to(payload).emit('currentPlayer', io.sockets.adapter.rooms[payload].length)
+      } else {
+        // jangan pindah routing
+        // roomname.push(payload + "1")
+        // socket.join(payload + "1");
+      }
+    } 
+  })
 
   socket.on('addPlayer', (payload) => {
     player = payload
     socket.emit('username', player)
   })
-  
-  socket.on('attack', (payload) => {
-    hp = hp - payload
-    io.emit('currentHp', payload)
+
+  socket.on('attack', ({ room, payload }) => {
+    console.log(room, payload);
+    io.to(room).emit('currentHp', payload)
     socket.emit('myScore', payload)
+    socket.emit('init', payload)
   })
 })
 
