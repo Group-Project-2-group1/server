c0d3e4a0baca4e8109db21ab9c6cc11f1ffcaa38
ekydpa21 <ekydodipratamaa21@gmail.com>
2021-01-21 20:56:09 +0700

    main game play

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const port = process.env.PORT || 3000
 
+let hp = 10
+
 io.on('connection', (socket) => {
   console.log('Socket.io client connected');
-  // socket.emit()
+  socket.emit('init', hp)
+  
+  socket.on('attack', (payload) => {
+    hp = hp - payload
+    io.emit('currentHp', payload)
+    socket.emit('myScore', payload)
+  })
 })
 
 server.listen(port, () => {
